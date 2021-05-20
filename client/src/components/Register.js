55c0c94b02eaa950d74900888c959ba6f7ecda33
dwinkler <danielbwo@gmail.com>
2021-05-20 13:13:21 +0200

    empezamos el register form y el footer

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 export default function Register() {
     return (
-        <div>
+        <div className="registerform">
+            <form action="#" method="POST">
+                <label htmlFor="newFirstName">First Name:</label>
+                <input type="text" name="newFirstName" id="newFirstName" required/>
+                <label htmlFor="newLastName">Last Name:</label>
+                <input type="text" name="newLastName" id="newLastName" required/>
+                <label htmlFor="newEmail">Email:</label>
+                <input type="email" name="newEmail" id="newEmail" required/>
+                <label htmlFor="newPassword">Password:</label>
+                <input type="password" name="newPassword" id="newPassword" required/>
+                <label htmlFor="newAccountType">Account Type:</label>
+                <select name="newAccountType" id="newAccountType" required>
+                    <option value="" hidden></option>
+                    <option value="user" name="newUser">User</option>
+                    <option value="shop" name="newShop">Shop</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
             
         </div>
     )
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Login from '../components/Login'
 
@@ -7,6 +8,7 @@ export default function LoginPage() {
         <div>
             <Header />
             <Login />
+            <Footer />
         </div>
     )
 }
