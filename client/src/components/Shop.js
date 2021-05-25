ca7b78229b01b630b1489d092e8f7e435fc34b09
dwinkler <danielbwo@gmail.com>
2021-05-25 12:55:40 +0200

    componente Shop para detalles de tienda y probando con una mock api

---
+++ client/src/components/Shop.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+export default function Shop() {
+
+    const [shop, setShop] = useState([])
+
+    const SHOP_URL = "https://run.mocky.io/v3/5cc0f585-70bb-4172-bfcf-763593f2d30c";
+
+    useEffect(() => {
+        fetch(SHOP_URL)
+        .then(r => r.json())
+        .then(data => setShop(data))
+    }, [])
+
+    return (
+        <>
+            <div className="shop">
+                <h2>{shop.name}</h2>
+                <h3>{shop.location}</h3>
+                <p>{shop.overview}</p>
+            </div>
+            <div className="shop">
+                <h4>Certificate Agencies</h4>
+                <p>{shop.cert_agencies?.map(agency => {
+                    return (
+                        <span>{agency}</span>
+                    )
+                })}</p>
+                <h4>Languages:</h4>
+                <p>{shop.languages?.map(language => {
+                    return (
+                        <span>{language}</span>
+                    )
+                })}</p>
+            </div>
+        </>
+    )
+}
--- client/src/pages/ShopPage.js
+++ client/src/pages/ShopPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Shop from '../components/Shop'
 
 export default function ShopPage() {
     return (
         <div>
-            Shop Page
+            <Shop />
         </div>
     )
 }
