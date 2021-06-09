5ac61da51a75ba597ebb004f421d9cf625c963b2
dwinkler <danielbwo@gmail.com>
2021-06-09 18:40:53 +0200

    empezamos a rellenar componente Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import placeholder from "../images/placeholder.jpg";
+import { countryCodeEmoji } from 'country-code-emoji'; // https://www.npmjs.com/package/country-code-emoji
 
-export default function Card() {
+
+export default function Card({shop}) {
     return (
-        <div>
-            
+        <div className="card">
+            <img src={placeholder} width={200} alt={"Shop"}/>
+            <h3>{shop.name}</h3>
+            <h4>{shop.location}</h4>
+            <p>{shop.languages?.map(lang => {
+                return( <span>{countryCodeEmoji(lang)}</span> )
+            })}</p>
         </div>
     )
 }
