3f0cc5b50fee407db17bc5d20ecbf0c24b7d1ab1
dwinkler <danielbwo@gmail.com>
2021-06-09 18:34:02 +0200

    modificamos el json para ajustarnos al paquete npm country-code-emoji

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-const SEARCH_URL = "https://run.mocky.io/v3/366cee3e-c002-418f-8a6f-f6d9dbff9c89"; // https://9f83c9ab-7ee6-4bbd-879d-36f5754b7816.mock.pstmn.io/
+const SEARCH_URL = "https://run.mocky.io/v3/298d5b76-d808-4995-8675-7039816d42bd"; // https://9f83c9ab-7ee6-4bbd-879d-36f5754b7816.mock.pstmn.io/
 const SHOP_URL = "https://run.mocky.io/v3/87d3cbad-cdb0-4a55-adba-17ed29dfce25"; // https://ae27b9c3-c7f2-4eba-a4b2-4221fea12dd8.mock.pstmn.io/
 
 export { 
