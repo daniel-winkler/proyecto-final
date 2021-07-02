97c8cc6ffc84b2b87c7c5cf594d247dfc9b097f2
dwinkler <danielbwo@gmail.com>
2021-07-02 14:13:48 +0200

    corregimos que la filterbox no se expanda innecesariamente

--- src/components/searchpage/Filter.js
+++ src/components/searchpage/Filter.js
@@ -4,12 +4,12 @@ import DestinationFilter from './filters/DestinationFilter';
 
 export default function Filter() {
 
-    // TODO: corregir que la filterbox se expanda hasta el footer
     return (
         <aside className="filterbox">
             <h3>Filter By:</h3>
             <LanguageFilter />
             <DestinationFilter />
+            <button>Clear all</button>
         </aside>
     )
 }
