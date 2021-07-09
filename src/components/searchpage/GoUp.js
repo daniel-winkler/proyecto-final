342c7aefada79f5cc53e3f75af07782fdbe2edca
dwinkler <danielbwo@gmail.com>
2021-07-09 17:03:46 +0200

    corregimos el error de className

--- src/components/searchpage/GoUp.js
+++ src/components/searchpage/GoUp.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function GoUp() {
     return (
         <div className="goup divebox">
-            <a href="#top"><i class="fas fa-arrow-up"></i></a>
+            <a href="#top"><i className="fas fa-arrow-up"></i></a>
         </div>
     )
 }
