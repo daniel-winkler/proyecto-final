b48de6fc5c04c6ce039e4ffcaefa9cdbbe0a617c
dwinkler <danielbwo@gmail.com>
2021-07-11 17:52:40 +0200

    hacemos que nos redirija a la pagina login cuando hacemos logout

--- src/components/dashboardpage/LogoutButton.js
+++ src/components/dashboardpage/LogoutButton.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useAuthContext } from '../../contexts/AuthContext'
+import { useHistory } from 'react-router';
 
 export default function LogoutButton() {
 
     const { signOut } = useAuthContext();
+    const history = useHistory();
+
+    function handleSignOut() {
+        signOut();
+        history.push("/login");
+    }
 
     return (
-        <div className="logout" onClick={()=>signOut()}>
+        <div className="logout" onClick={()=>handleSignOut()}>
             <button>Logout</button>
         </div>
     )
