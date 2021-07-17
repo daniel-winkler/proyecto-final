e63404c538323f4c7c87aeb4115ce41508141028
dwinkler <danielbwo@gmail.com>
2021-07-17 17:43:03 +0200

    modificamos el footer a algo mas personalizado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer>
-            <p>SCUBA BUDDY &copy; 2021</p>
+            <p>Made with<i class="fas fa-heart"></i>in Málaga</p>
         </footer>
     )
 }
