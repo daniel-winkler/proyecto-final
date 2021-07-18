7ce025baf9a3d9b46019498dbdc27b1b9b70c4c1
dwinkler <danielbwo@gmail.com>
2021-07-18 10:19:51 +0200

    hacemos funcionar el boton clear que nos resetea todos los estados de parametros de busqueda

--- src/components/searchpage/Filter.js
+++ src/components/searchpage/Filter.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import LanguageFilter from './filters/LanguageFilter';
 import DestinationFilter from './filters/DestinationFilter';
+import { useParamsContext } from '../../contexts/ParamsContext';
 
 export default function Filter() {
+
+    const { setDestination, setInput, setLanguage, setPage } = useParamsContext();
+
+    function handleClear(){
+        setPage("")
+        setLanguage("")
+        setInput("")
+        setDestination("")
+    }
 
     return (
         <aside className="filterbox">
             <h3>Filter By:</h3>
             <LanguageFilter/>
             <DestinationFilter/>
-            <button>Clear all</button>
+            <button onClick={()=>handleClear()}>Clear all</button>
         </aside>
     )
 }
