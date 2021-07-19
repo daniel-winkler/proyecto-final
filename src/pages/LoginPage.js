9fd6f5e6c96498b991c1b6d4f2351f12030116f3
dwinkler <danielbwo@gmail.com>
2021-07-19 17:15:29 +0200

    en el caso de refrescar pagina nos redirige al dashboard si estamos authenticados

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import Login from '../components/Login'
+import { useEffect } from 'react'
+import { useHistory } from 'react-router'
+import { useAuthContext } from '../contexts/AuthContext'
 
 export default function LoginPage() {
+
+    const { isAuthenticated } = useAuthContext();
+    const history = useHistory();
+
+    useEffect(() => {
+        isAuthenticated && history.push("/dashboard")
+    }, [isAuthenticated])
+
     return (
         <div>
             <Login />
