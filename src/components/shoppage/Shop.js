9ea055869bf6719ddc6d7907f87ee03a5c60a748
dwinkler <danielbwo@gmail.com>
2021-07-19 18:27:31 +0200

    añadimos la imagen en la pagina de detalles

--- src/components/shoppage/Shop.js
+++ src/components/shoppage/Shop.js
@@ -3,8 +3,7 @@ import { useParams } from "react-router-dom";
 import { countryCodeEmoji } from 'country-code-emoji'; // https://www.npmjs.com/package/country-code-emoji
 import Map from './Map';
 import Contactform from './Contactform';
-import { DETAILS_URL } from "../../config";
-
+import { DETAILS_URL, SHOP_IMAGES_URL } from "../../config";
 
 export default function Shop() {
 
@@ -23,12 +22,18 @@ export default function Shop() {
             <div className="shop">
                 <h2>{shop.name}</h2>
                 <h3>{shop.location}</h3>
+                {shop.image && <img src={SHOP_IMAGES_URL + shop.image} alt="Shop"/>}
             </div>
             <div className="shop">
-                <h4>Languages:</h4>
-                <p>{shop.languages?.map(language => {
-                    return ( <span key={language.id} className="flags">{countryCodeEmoji(language.countrycode)}</span> )
-                })}</p>
+                <div>
+                    <h4>Languages:</h4>
+                    <p>{shop.languages?.map(language => {
+                        return ( <span key={language.id} className="flags">{countryCodeEmoji(language.countrycode)}</span> )
+                    })}</p>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </div>
             <div className="shop">
                 <h4>Location</h4>
